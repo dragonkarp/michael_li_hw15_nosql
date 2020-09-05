2c2f0442099a236f04e9d7af4cf609687420bbd9
Michael Li <dev.mike011235@gmail.com>
2020-09-04 21:55:16 -0400

    Implemented workout model

---
+++ Develop/models/workouts.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutsSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: "Enter your workout name."
+    },
+    type: {
+        type: String,
+        trim: true,
+        required: "Enter the type of the workout."
+    },
+    weight: {
+        type: Number,
+        default: 0
+    },
+    sets: {
+        type: Number,
+        default: 0
+    },
+    reps: {
+        type: Number,
+        default: 0
+    },
+    duration: {
+        type: Number,
+        default: 0
+    }
+})
+
+const Workouts = mongoose.model("Workouts", workoutsSchema);
+
+module.exports = Workouts;
